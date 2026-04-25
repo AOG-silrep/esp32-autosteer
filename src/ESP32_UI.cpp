a010b755ee7bd247fc405d10179c87c2c0c4b2d1
Reuben Rissler <benreu.rissler@gmail.com>
2026-04-25 12:04:34 -0400

    only show HMS widget when Canbus is enabled

--- src/ESP32_UI.cpp
+++ src/ESP32_UI.cpp
@@ -129,13 +129,15 @@ void initESPUI ( void ) {
       ESPUI.addControl( ControlType::Option, "500kB/s", "500", ControlColor::Alizarin, sel );
     }
 
-    {
-      uint16_t sel = ESPUI.addControl( ControlType::Select, "HMS version*", String( ( int )steerConfig.canbusHmsVersion ), ControlColor::Peterriver, tab,
-      []( Control * control, int id ) {
-        steerConfig.canbusHmsVersion = ( SteerConfig::HmsVersion )control->value.toInt();
-      } );
-      ESPUI.addControl( ControlType::Option, "None", "0", ControlColor::Alizarin, sel );
-      ESPUI.addControl( ControlType::Option, "Deere 0x18FFFA21", "1", ControlColor::Alizarin, sel );
+    if( steerConfig.canBusEnabled ){
+      {
+        uint16_t sel = ESPUI.addControl( ControlType::Select, "HMS version*", String( ( int )steerConfig.canbusHmsVersion ), ControlColor::Peterriver, tab,
+        []( Control * control, int id ) {
+          steerConfig.canbusHmsVersion = ( SteerConfig::HmsVersion )control->value.toInt();
+        } );
+        ESPUI.addControl( ControlType::Option, "None", "0", ControlColor::Alizarin, sel );
+        ESPUI.addControl( ControlType::Option, "Deere 0x18FFFA21", "1", ControlColor::Alizarin, sel );
+      }
     }
   }
 
